9f462d93a30c0515491369674a9f6538c007c8fa
ChristopheKunzli <christophe.kunzli@cpnv.ch>
2024-10-28 17:06:58 +0100

    very basic connection

--- code/ChristopheKunzli_TriGerber/Server/src/main/java/ch/heig/dai/lab/protocoldesign/Server.java
+++ code/ChristopheKunzli_TriGerber/Server/src/main/java/ch/heig/dai/lab/protocoldesign/Server.java
@@ -1,14 +1,38 @@
 package ch.heig.dai.lab.protocoldesign;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
     final int SERVER_PORT = 1234;
+
+    private ServerSocket serverSocket;
 
     public static void main(String[] args) {
         // Create a new server and run it
         Server server = new Server();
+        try {
+            server.serverSocket = new ServerSocket(server.SERVER_PORT);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         server.run();
     }
 
     private void run() {
-    } 
+        try (Socket clientSocket = serverSocket.accept()) {
+            var in = new BufferedInputStream(clientSocket.getInputStream());
+            var out = new BufferedOutputStream(clientSocket.getOutputStream());
+
+            System.out.println("Client connected");
+            out.write("Hello from server\n".getBytes());
+            out.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
